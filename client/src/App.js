b435ba10d2ee2a72f538c8a4deaa7cf371745602
Jenn Nguyen <jennguy17@gmail.com>
2022-04-02 19:39:31 -0700

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testing for Studio Ghibli Website</h1>
+    </div>
+  );
+}
+
+export default App;
