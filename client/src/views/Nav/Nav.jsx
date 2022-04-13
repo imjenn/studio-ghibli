44d6260745c1ee8ec898231cc009aae082c3de06
Jenn Nguyen <jennguy17@gmail.com>
2022-04-12 23:36:28 -0700

    Added responsiveness to navbar

--- client/src/views/Nav/Nav.jsx
+++ client/src/views/Nav/Nav.jsx
@@ -1,8 +1,27 @@
+import { useState } from 'react';
 import Cart from '../Cart/CartIcon';
 import './Nav.css';
 import cart from '../../assets/cart.jpeg';
+import menu from '../../assets/menu-svgrepo-com.svg'
 
 const Nav = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const dropMenuHandler = () => {
+        if (!openMenu) {
+            setOpenMenu(true);
+        } else {
+            setOpenMenu(false);
+        }
+    }
+
+    window.addEventListener('mouseup', function(e) {
+        var x = document.querySelector('#dropdown-menu');
+        if (!document.getElementById('dropdown-menu').contains(e.target)) {
+            x.style.display = "none";
+        }
+    });
+
     return (
         <nav>
             <a href="/">
@@ -19,15 +38,36 @@ const Nav = () => {
                     <li>
                         <a href="">Shop</a>
                     </li>
+                    <li>
+                        <a href="">Music</a>
+                    </li>
                 </ul>
                 <div className="nav-cart">
                     <a href="/register">Login/Register</a>
                     <p>
                         <Cart />
-                        <img src={cart} alt=""/>
+                        <img src={cart} alt="" />
                     </p>
                 </div>
+                <div className="menu" onClick={dropMenuHandler}>
+                    <img
+                        src={menu}
+                        alt="hamburger menu icon"
+                    />
+                </div>
             </div>
+            {
+                openMenu &&
+                <div id="dropdown-menu">
+                    <ul>
+                        <li>Films</li>
+                        <li>About</li>
+                        <li>Shop</li>
+                        <li>Lofi</li>
+                        <li>Login/Register</li>
+                    </ul>
+                </div>
+            }
         </nav>
     )
 }
